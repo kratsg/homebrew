0a57bbdc6ba4663c933f2a57b1d857ec23058912
Giordon Stark <kratsg@gmail.com>
2018-04-01 09:19:28 -0500

    don't use fortran, switch to gcc

--- contin.rb
+++ contin.rb
@@ -5,7 +5,7 @@ class Contin < Formula
   version "1.0.0"
   sha256 "57bfad6621edc351dfd2fe7627aa78b39d5d8b963ee18d621e8fce314f2dfb53"
 
-  depends_on :fortran
+  depends_on "gcc"
 
   def install
     system ENV.fc, "-o", "contin-cd", "contin-cd.for"
